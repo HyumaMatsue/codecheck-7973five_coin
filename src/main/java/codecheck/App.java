77b7be101c08fc5f392176f2268ad9ef4f105de6
hyuma <hyuma@hyuma-no-MacBook-Air.local>
2017-03-23 03:09:41 +0900

    first

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,24 +1,30 @@
 package codecheck;
-import java.io.*;
 import java.util.Scanner;
 
 
-public class App {
-  int[] coin_math = new int[5];
-  int cost_count;
-	public static void main(String[] args) {
-      	//cost_count = 0;
-       
-		int s[]=new int[5];
-		//Scannerクラスのインスタンスの生成
-		Scanner sc = new Scanner(System.in);
-		for(int i=0;i<s.length;i++){
-			System.out.println("整数を入力");
-			//整数をキーボードから入力し、配列に記憶
-			s[i]= sc.nextInt();
-		}
-		for(int i=0;i<s.length;i++){
-			System.out.printf("s[%2d]=%d\n",i,s[i]); 
-		} 
-	}
+public class App{
+    
+    public static void main(String[] args) {
+        int[] coin_math = new int[5];
+        int cost_count = 0;
+        
+        int s[]=new int[5];
+        //Scannerクラスのインスタンスの生成
+        Scanner sc = new Scanner(System.in);
+        System.out.print("コイン：[");
+        for(int i=0;i<s.length;i++){
+            
+            //整数をキーボードから入力し、配列に記憶
+            s[i]= sc.nextInt();
+            if( i == s.length - 1 ){
+                System.out.print("]");
+            }else{
+                System.out.print(",");
+            }
+        }
+        
+        //Scanner count = new Scanner(System.in);
+       // System.out.println("金額：" + sc.nextInt);
+        
+    }
 }
